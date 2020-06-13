65265bdd8fa176b984438bf15b34d674001f1e76
Nathan Chung <chung.nathanh@gmail.com>
2020-06-13 15:22:45 +0000

    Automatically backed up by Learn

--- fix_using_tests/the_final_frontier.rb
+++ fix_using_tests/the_final_frontier.rb
@@ -8,11 +8,7 @@ def state_log(star_date)
   "Captain's Log, star date #{star_date}."
 end
 
-def crew
-  ["Geordi", "Data", "Worf", "William", "Beverly", "Deanna"]
-end
-
 def engage
   date = generate_star_date
-  state_log(date)
+  puts state_log(date)
 end
