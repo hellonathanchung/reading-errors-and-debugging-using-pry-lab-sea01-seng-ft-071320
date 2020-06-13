8ff7ab8e51d8525b8ae22554ff314b4710be3b14
Nathan Chung <chung.nathanh@gmail.com>
2020-06-13 15:20:22 +0000

    Automatically backed up by Learn

--- fix_using_tests/the_final_frontier.rb
+++ fix_using_tests/the_final_frontier.rb
@@ -7,5 +7,16 @@ def state_log(star_date)
   "Captain's Log, star date #{star_date}."
 end
 
+def crew
+  ["Geordi", "Data", "Worf", "William", "Beverly", "Deanna"]
+end
+
+def greet_crew (crew)
+  crew.map {|crew_member| "Hello #{crew_member}."}
+end
+
 def engage
+  date = generate_star_date
+  state_log(date)
+  greet_crew(crew)
 end
